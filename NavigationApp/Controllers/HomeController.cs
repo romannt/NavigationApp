a279b0cbf044c0cc631a31649e45c3369f885731
Roman Tyshchenko <romannt@ukr.net>
2017-09-14 00:45:35 +0300

    Add project files.

---
+++ NavigationApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using NavigationApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace NavigationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        SoccerContext db = new SoccerContext();
+
+        public ActionResult Index()
+        {
+            // Если не подключено пространство имён System.Data.Entity студия считает параметр ошибочным
+            var players = db.Players.Include(p => p.Team);
+            return View(players.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NavigationApp/Models/SoccerContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace NavigationApp.Models
+{
+    public class SoccerContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
